90dbcf9f1637313a1ee0648b0c8c484e32fea6f2
007devang <102285817+007devang@users.noreply.github.com>
2023-03-23 12:27:04 +0530

    docker-file configuration

--- apps/hierarchy/src/main.ts
+++ apps/hierarchy/src/main.ts
@@ -3,6 +3,6 @@ import { HierarchyModule } from './hierarchy.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(HierarchyModule);
-  await app.listen(3000);
+  await app.listen(3003);
 }
 bootstrap();
